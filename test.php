ad0e87a549165ca2630276957c662dc1cf21c268
Dr. Bryan Marshall <professor@bryanmarshall.com>
2018-03-13 10:35:21 -0400

    br

--- test.php
+++ test.php
@@ -4,16 +4,10 @@ echo "hello world Bryan ";
 
 echo "hello world Tori ";
 echo "hello world Tori ";
-<<<<<<< HEAD
 
+echo "hello world Bryan ";
 echo "hello world Tori ";
-echo "hello world Tori ";
-echo "hello world Tori ";
-
-
-
-=======
->>>>>>> 0de758426fb8e2d278b3601139be4ea9f61778ea
+echo "hello world Bryan ";
 
 
  ?>
